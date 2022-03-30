7840f208a219d8c99e87674816d7c14293e62820
Adryan Cadet <adryan-pro@hotmail.com>
2022-03-30 11:43:05 +0200

    Iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import AllContacts from "./contacts.json";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const firstFive = AllContacts.slice(30, 35);
+  const [celebs, setCelebs] = useState(firstFive);
+  console.log(firstFive);
+
+  // const addRandom = () => {
+  //   const randomIndex = Math.floor(Math.random() * AllContacts.length);
+  //   const setCelebrities = new Set(celebs);
+  //   setCelebs([AllContacts[randomIndex], ...celebs]);
+  // };
+
+  const addRandom = () => {
+    const random = AllContacts[Math.floor(Math.random() * AllContacts.length)];
+    if (celebs.find((celebs) => celebs.id === random.id)) {
+      if (celebs.length < AllContacts.length) {
+        addRandom();
+      }
+      return;
+    }
+    setCelebs((celebs) => [random, ...celebs]);
+  };
+
+  const sortByName = () => {
+    const deepCopy = JSON.parse(JSON.stringify(celebs));
+    deepCopy.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    setCelebs(deepCopy);
+  };
+
+  const sortByPop = () => {
+    const deepCopy = JSON.parse(JSON.stringify(celebs));
+    deepCopy.sort((a, b) => {
+      if (a.popularity < b.popularity) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    setCelebs(deepCopy);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Contacts</h1>
+      <button onClick={sortByName}>Sort By Name</button>
+      <button onClick={sortByPop}>Sort By Popularity</button>
+      <button onClick={addRandom}>Add Random Contact</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th className="betterLook">Won Oscar</th>
+            <th className="betterLook">Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {celebs.map((el, index) => {
+            return (
+              <tr>
+                <td>
+                  <img
+                    src={el.pictureUrl}
+                    alt="celebrities Pictures"
+                    style={{ height: "200px" }}
+                  />
+                </td>
+                <td>
+                  <h3>{el.name}</h3>
+                </td>
+                <td>
+                  <h3>{el.popularity}</h3>
+                </td>
+                <td>
+                  {el.wonOscar ? <h3 className="betterLookH3">🏆</h3> : null}
+                </td>
+                <td>
+                  <h3>
+                    {el.wonEmmy ? <h3 className="betterLookH3">🏆</h3> : null}
+                  </h3>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
